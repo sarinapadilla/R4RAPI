ba26bf328ef5dcadf47600100f2581050365c5ca
Sarina Padilla <sarinapadilla@users.noreply.github.com>
2018-04-18 10:20:06 -0400

    Adding R4RAPI index to config. Adding instance of elastic client to startup.

---
+++ src/R4RAPI/Models/R4RAPIOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace R4RAPI.Models
+{
+    public class R4RAPIOptions
+    {
+        public string AliasName { get; set; }
+    }
+}
