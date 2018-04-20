93aa292ab5d8b91011b1ad3f8661b487b97d2f41
Bryan Pizzillo <bryanp@mail.nih.gov>
2018-04-20 09:11:55 -0400

    Simplified AggSvc now that we flattened tool types Added needed comments to resources.

--- src/R4RAPI/Models/Resource.cs
+++ src/R4RAPI/Models/Resource.cs
@@ -6,42 +6,93 @@ using Nest;
 
 namespace R4RAPI.Models
 {
+    /// <summary>
+    /// Describes the information about a resource
+    /// </summary>
     [ElasticsearchType(Name = "resource")]
     public class Resource
     {
+        /// <summary>
+        /// The resource identifier
+        /// </summary>
+        /// <value>The identifier.</value>
         [Number(NumberType.Integer, Name = "id")]
         public int ID { get; set; }
 
+        /// <summary>
+        /// The detailed description of the resource
+        /// </summary>
+        /// <value>The body.</value>
         [Keyword(Name = "body")]
         public string Body { get; set; }
 
+        /// <summary>
+        /// The title of the resource
+        /// </summary>
+        /// <value>The title.</value>
         [Keyword(Name = "title")]
         public string Title { get; set; }
 
+        /// <summary>
+        /// The URL where you can get more information about the resource
+        /// </summary>
+        /// <value>The website.</value>
         [Keyword(Name = "website")]
         public string Website { get; set; }
 
+        /// <summary>
+        /// A brief description of the resource
+        /// </summary>
+        /// <value>The description.</value>
         [Keyword(Name = "description")]
         public string Description { get; set; }
 
+        /// <summary>
+        /// A list of the tool types this resource is categorized as
+        /// </summary>
+        /// <value>The tool types.</value>
         [Nested(Name = "toolTypes")]
         public KeyLabel[] ToolTypes { get; set; }
 
+        /// <summary>
+        /// A list of the sub tool types this resource is categorized as
+        /// </summary>
+        /// <value>The tool subtypes.</value>
         [Nested(Name = "toolSubtypes")]
         public ToolSubtype[] ToolSubtypes { get; set; }
 
+        /// <summary>
+        /// A list of the areas of research areas where this resource applies
+        /// </summary>
+        /// <value>The research areas.</value>
         [Nested(Name = "researchAreas")]
         public KeyLabel[] ResearchAreas { get; set; }
 
+        /// <summary>
+        /// A list of the areas of research types where this resource applies
+        /// </summary>
+        /// <value>The research types.</value>
         [Nested(Name = "researchTypes")]
         public KeyLabel[] ResearchTypes { get; set; }
 
+        /// <summary>
+        /// Information about how one can aquire access to the resource
+        /// </summary>
+        /// <value>The resource access.</value>
         [Nested(Name = "resourceAccess")]
         public ResourceAccess ResourceAccess { get; set; }
 
+        /// <summary>
+        /// A list of the NCI Divisions, Offices and Centers (DOCs) that manage the resource
+        /// </summary>
+        /// <value>The document.</value>
         [Nested(Name = "docs")]
         public KeyLabel[] DOCs { get; set; }
 
+        /// <summary>
+        /// A list of points of contact for the resource
+        /// </summary>
+        /// <value>The POC.</value>
         [Nested(Name = "pocs")]
         public Contact[] POCs { get; set; }
     }
--- src/R4RAPI/Services/ESResourceAggregationService.cs
+++ src/R4RAPI/Services/ESResourceAggregationService.cs
@@ -46,7 +46,7 @@ namespace R4RAPI.Services
                 // Let's check if the field has a period if it does, then we need
                 // to handle it differently because it is either toolType.type or
                 // toolType.subtype.
-                Aggregations = field.Contains(".") ? GetComplexAgg(field, query) : GetSimpleAgg(field)
+                Aggregations = GetAggQuery(field, query)
             };
 
             try
@@ -55,11 +55,8 @@ namespace R4RAPI.Services
                 //even though our queries should not return anything.
                 var res = this._elasticClient.Search<Resource>(req);
 
-                if (field.Contains(".")) {
-                    return ExtractComplexAggs(field, res).ToArray();
-                } else {
-                    return ExtractSimpleAggs(field, res).ToArray();
-                }
+
+                return ExtractAggResults(field, res).ToArray();
             } catch (Exception ex) {
                 this._logger.LogError($"Could not fetch aggregates for field: {field}");
                 throw new Exception($"Could not fetch aggregates for field: {field}", ex);
@@ -72,7 +69,7 @@ namespace R4RAPI.Services
         /// <returns>The simple aggs.</returns>
         /// <param name="field">Field.</param>
         /// <param name="res">Res.</param>
-        private IEnumerable<KeyLabelAggResult> ExtractSimpleAggs(string field, ISearchResponse<Resource> res)  {
+        private IEnumerable<KeyLabelAggResult> ExtractAggResults(string field, ISearchResponse<Resource> res)  {
             var nested = res.Aggs.Nested($"{field}_agg");
 
             var keys = nested.Terms($"{field}_key");
@@ -94,52 +91,16 @@ namespace R4RAPI.Services
                     Count = count
                 };
             }
-        } 
-
-        private IEnumerable<KeyLabelAggResult> ExtractComplexAggs(string field, ISearchResponse<Resource> res)  {
-            throw new NotImplementedException();
-        } 
-
+        }
 
         /// <summary>
         /// Gets the simple aggregation "query"
         /// </summary>
         /// <returns>An AggregationDictionary containing the "query"</returns>
         /// <param name="field">Field to aggregate</param>
-        private AggregationDictionary GetSimpleAgg(string field)
+        /// <param name="query"></param>
+        private AggregationDictionary GetAggQuery(string field, ResourceQuery query)
         {
-            //Start with a nested aggregation
-            var agg = new NestedAggregation($"{field}_agg")
-            {
-                Path = new Field(field), //Set the path of the nested agg
-                                         // Add the sub aggregates (bucket keys)
-                Aggregations = new TermsAggregation($"{field}_key")
-                {
-                    Field = new Field($"{field}.key"), //Set the field to rollup
-                    Size = 999, //Use a large number to indicate unlimted (def is 10)
-                                // Now, we need to get the labels for the keys and thus
-                                // we need to add a sub aggregate for this term.  
-                                // Normally you would do this for something like city/state rollups
-                    Aggregations = new TermsAggregation($"{field}_label")
-                    {
-                        Field = new Field($"{field}.label")
-                    }
-                }
-            };
-
-            return agg;
-        }
-
-        /// <summary>
-        /// Gets a complex (e.g. tooltype) simple aggregation "query"
-        /// </summary>
-        /// <returns>An AggregationDictionary containing the "query"</returns>
-        /// <param name="field">Field to aggregate</param>
-        /// <param name="query">The query object.</param>
-        private AggregationDictionary GetComplexAgg(string field, ResourceQuery query)
-        {
-            // this is for things like tooltype.
-
             //Start with a nested aggregation
             var agg = new NestedAggregation($"{field}_agg")
             {
