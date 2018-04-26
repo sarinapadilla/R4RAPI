f3b6a32d94d1846804419e16a7dbe63a3a2bbcf3
Sarina Padilla <sarinapadilla@users.noreply.github.com>
2018-04-26 11:04:17 -0400

    Adding FluentAssertions package for easier unit testing (maybe?)

---
+++ test/R4RAPI.Test/Tests/Models/ResourceQueryResultComparer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using R4RAPI.Models;
+
+namespace R4RAPI.Test.Models
+{
+    public class ResourceQueryResultComparer : IEqualityComparer<ResourceQueryResult>
+    {
+        public bool Equals(ResourceQueryResult x, ResourceQueryResult y)
+        {
+            if (x == y)
+                return true;
+
+            if (x == null || y == null)
+                return false;
+
+            return false;
+        }
+
+        public int GetHashCode(ResourceQueryResult obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
