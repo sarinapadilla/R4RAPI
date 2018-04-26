7de7a5a7a405dc9c3c12d966d6276247c3bf014d
Bryan Pizzillo <bryanp@mail.nih.gov>
2018-04-26 10:36:17 -0400

    Filled in Resource 101

--- test/R4RAPI.Test/Tests/Services/TestDataObjects/ResourceQuerySvc/StaticResourceData.cs
+++ test/R4RAPI.Test/Tests/Services/TestDataObjects/ResourceQuerySvc/StaticResourceData.cs
@@ -11,8 +11,15 @@ namespace R4RAPI.Test.Services
                 ID = 101,
                 Title = "NCI Data Catalog",
                 Website = "https://www.cancer.gov/research/resources/data-catalog",
+                ResourceAccess = new ResourceAccess { Type = "open" },
                 ToolTypes = new KeyLabel[] {
                     new KeyLabel{ Label = "Datasets & Databases", Key = "datasets_databases"}
+                },
+                ToolSubtypes = new ToolSubtype[] {
+                    new ToolSubtype{ Label = "Clinical Data", Key = "clinical_data", ParentKey = "datasets_databases" },
+                    new ToolSubtype{ Label = "Epidemiologic Data", Key = "epidemiologic_data", ParentKey = "datasets_databases" },
+                    new ToolSubtype{ Label = "Genomic Datasets", Key = "genomic_datasets", ParentKey = "datasets_databases" },
+                    new ToolSubtype{ Label = "Imaging", Key = "imaging", ParentKey = "datasets_databases" }
                 },
                 ResearchAreas = new KeyLabel[] {
                     new KeyLabel{ Key = "cancer_biology",Label = "Cancer Biology" },
@@ -21,76 +28,29 @@ namespace R4RAPI.Test.Services
                     new KeyLabel{ Key = "screening_detection", Label = "Screening & Detection" },
                     new KeyLabel{ Key = "cancer_treatment", Label = "Cancer Treatment" }
                 },
-
+                ResearchTypes = new KeyLabel[] {
+                    new KeyLabel{ Key = "basic", Label = "Basic" },
+                    new KeyLabel{ Key = "clinical", Label = "Clinical" },
+                    new KeyLabel{ Key = "epidemiologic", Label = "Epidemiologic" },
+                    new KeyLabel{ Key = "translational", Label = "Translational" }
+                },
+                DOCs = new KeyLabel[] {
+                    new KeyLabel { Key = "cbiit", Label = "Center for Biomedical Informatics and Informatics Technology (CBIIT)" }
+                },
+                POCs = new Contact[] {
+                    new Contact{
+                        Name = new Name {
+                            FirstName = "Mervi",
+                            LastName = "Heiskanen"
+                        },
+                        Email = "Mervi.Heiskanen@nih.gov",
+                        Phone = "240-276-5175",
+                        Title = "Program Manager NCI CBIIT"
+                    }
+                },
+                Body = "<p>The NCI Data Catalog is a listing of data collections produced by major NCI initiatives and other widely used data sets. Data collections included in the catalog meet the following criteria: Produced by NCI intramural researchers or major NCI initiatives, regularly referenced NCI funded extramural research data, available to all researchers and may be Open or Controlled Access (requiring approval by a Data Access Committee), well-documented and available for download.</p>",
+                Description = ""
             };
-
-            /*
-{
-    "researchTypes": [
-      {
-        "key": "basic",
-        "label": "Basic"
-      },
-      {
-        "key": "clinical",
-        "label": "Clinical"
-      },
-      {
-        "key": "epidemiologic",
-        "label": "Epidemiologic"
-      },
-      {
-        "key": "translational",
-        "label": "Translational"
-      }
-    ],
-    "resourceAccess": { "type": "open" },
-    "docs": [
-      {
-        "key": "cbiit",
-        "label": "Center for Biomedical Informatics and Informatics Technology (CBIIT)"
-      }
-    ],
-    "body": "<p>The NCI Data Catalog is a listing of data collections produced by major NCI initiatives and other widely used data sets. Data collections included in the catalog meet the following criteria: Produced by NCI intramural researchers or major NCI initiatives, regularly referenced NCI funded extramural research data, available to all researchers and may be Open or Controlled Access (requiring approval by a Data Access Committee), well-documented and available for download.</p>",
-    "description": "",
-    "pocs": [
-      {
-        "email": "Mervi.Heiskanen@nih.gov",
-        "name": {
-          "firstname": "Mervi",
-          "lastname": "Heiskanen"
-        },
-        "phone": "240-276-5175",
-        "title": "Program Manager NCI CBIIT"
-      }
-    ],
-    "toolSubtypes": [
-      {
-        "label": "Clinical Data",
-        "key": "clinical_data",
-        "parentKey": "datasets_databases"
-      },
-      {
-        "label": "Epidemiologic Data",
-        "key": "epidemiologic_data",
-        "parentKey": "datasets_databases"
-      },
-      {
-        "label": "Genomic Datasets",
-        "key": "genomic_datasets",
-        "parentKey": "datasets_databases"
-      },
-      {
-        "label": "Imaging",
-        "key": "imaging",
-        "parentKey": "datasets_databases"
-      }
-    ]
-  }
-}              
-             
-             */ 
-
         }
     }
 }
